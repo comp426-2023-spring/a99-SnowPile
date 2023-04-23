047b5f7c1aed1341e993957169a134b7d7aeda96
BryFReed <bfr7011@nyu.edu>
2023-04-22 21:24:42 -0400

    alot of changes, somthing kinda runs

---
+++ backend/create-tables.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+
+const createTables = () => {
+  const dbPath = path.resolve(__dirname, './databse.js');
+  const schemaPath = path.resolve(__dirname, './schema.sql');
+
+  const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+
+    console.log('Connected to the database.');
+
+    db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='users'", (err, row) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+
+      if (!row) {
+        const schema = fs.readFileSync(schemaPath, { encoding: 'utf-8' });
+
+        db.exec(schema, (err) => {
+          if (err) {
+            console.error(err.message);
+          } else {
+            console.log('Tables created successfully.');
+          }
+        });
+      }
+
+      db.close((err) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          console.log('Connection to the database closed.');
+        }
+      });
+    });
+  });
+};
+
+module.exports = createTables;
--- backend/server.js
+++ backend/server.js
@@ -1,87 +1,115 @@
-const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
-const md5 = require("md5");
+// Define app using express
+var express = require("express")
+var app = express()
+// Require database SCRIPT file
+const db = require("./database.js")
+const path = require('path');
+const ejs = require('ejs')
+app.set('view engine', 'ejs');
+const authRouter = require('./auth.js');
+const todoRouter = require('./todo.js');
+const createTables = require('./create-tables.js');
+const middlewares = require('./middlewares.js');
+const session = require('express-session');
+const SQLiteStore = require('connect-sqlite3')(session);
 
-const app = express();
 
-const DBSOURCE = "db.sqlite";
-const db = new sqlite3.Database(DBSOURCE, (err) => {
-  if (err) {
-    console.error(err.message);
-    throw err;
-  } else {
-    console.log("Connected to the SQLite database.");
-    db.run(
-      `CREATE TABLE IF NOT EXISTS user (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT, 
-        email TEXT UNIQUE, 
-        password TEXT
-      )`,
-      (err) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log("User table created successfully.");
-        }
-      }
-    );
-
-    db.run(
-      `CREATE TABLE IF NOT EXISTS task_list (
-        task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        task_title TEXT NOT NULL,
-        user_id INTEGER NOT NULL,
-        task_due_date DATETIME, 
-        task_status TEXT NOT NULL, 
-        task_description TEXT,
-        category_id INTEGER NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES user(id),
-        FOREIGN KEY (category_id) REFERENCES category(id)
-      )`,
-      (err) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log("Task list table created successfully.");
-        }
-      }
-    );
-  }
+app.get('/', function(req, res) {
+  res.render('login');
 });
 
-// Enable JSON request body parsing
-app.use(express.json());
+// parse form data middleware
+app.use(express.urlencoded({ extended: false }));
 
-// Add a new user
-app.post("/users", (req, res) => {
-  const { name, email, password } = req.body;
-  const hashedPassword = md5(password);
+// serve static files
+app.use(express.static('public'));
 
-  const insertUser = "INSERT INTO user (name, email, password) VALUES (?,?,?)";
-  db.run(insertUser, [name, email, hashedPassword], function (err) {
-    if (err) {
-      console.error(err.message);
-      res.status(500).json({ error: "Failed to add user" });
-    } else {
-      console.log(`User ${this.lastID} added successfully`);
-      res.status(201).json({ id: this.lastID });
+// set up session middleware
+app.use(session({store: new SQLiteStore({db:'./database.js',concurrentDB: true}),secret: 'secret',resave: false,saveUninitialized: true,cookie: { maxAge: 7 *24 * 60 * 60 * 1000 } }));
+
+app.use(middlewares.setCurrentUser);
+app.use('/auth', authRouter);
+app.use('/todo', middlewares.requireLogin, todoRouter);
+app.get('/', (req, res) => {
+  res.redirect('/auth/login');
+});
+
+//app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Route to serve the login page
+//app.get('/login', (req, res) => {
+//  ejs.renderFile(path.join(__dirname, '../frontend', 'login.ejs'), {}, (err, html) => {
+//    if (err) {
+//      console.error(err);
+//      res.status(500).send('Internal Server Error');
+//    } else {
+//      res.send(html);
+//    }
+//  });
+//});
+////
+createTables();
+
+// Server port
+var HTTP_PORT = 5555;
+// Start server
+const server = app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+
+// Define other CRUD API endpoints using express.js and better-sqlite3
+// CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.post("/app/new/user", (req, res, next) => {
+    let data = {
+        user: req.body.username,
+        pass: req.body.password
     }
-  });
+    const stmt = db.prepare('INSERT INTO user (username, password) VALUES (?, ?)')
+    const info = stmt.run(data.user, data.pass)
+    res.status(200).json(info)
+});
+// READ a list of users (HTTP method GET) at endpoint /app/users/
+app.get("/app/users", (req, res) => {
+    try {
+        const stmt = db.prepare('SELECT * FROM user').all()
+        res.status(200).json(stmt)
+    } catch {
+        console.error(e)
+    }
 });
 
-// Get all users
-app.get("/users", (req, res) => {
-  db.all("SELECT * FROM user", (err, rows) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).json({ error: "Failed to retrieve users" });
-    } else {
-      res.status(200).json(rows);
+// READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {
+    try {
+        const stmt = db.prepare('SELECT * FROM user WHERE id = ?').get(req.params.id);
+        res.status(200).json(stmt)
+    } catch (e) {
+        console.error(e)
     }
-  });
+
 });
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+
+// UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req, res) => {
+    let data = {
+        user: req.body.username,
+        pass: req.body.password
+    }
+    const stmt = db.prepare('UPDATE user SET username = COALESCE(?,username), password = COALESCE(?,password) WHERE id = ?')
+    const info = stmt.run(data.user, data.pass, req.params.id)
+    res.status(200).json(info)
 });
+
+// DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
+app.delete("/app/delete/user/:id", (req, res) => {
+    const stmt = db.prepare('DELETE FROM user WHERE id = ?')
+    const info = stmt.run(req.params.id)
+    res.status(200).json(info)
+});
+// Default response for any other request
+//app.use(function(req, res){
+//    res.json({"message":"Endpoint not found. (404)"});
+//    res.status(404);
+//});
+// set up routers
