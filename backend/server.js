082f0821a60b8d53091f6e32e4cc25d329bb3586
Ziqiao Ma <96596886+ZiqiaoM@users.noreply.github.com>
2023-05-07 17:47:18 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,9 +43,18 @@ app.get('/profile.ejs', function(req, res) {
 //    const user = req.body.user
 //    res.render('profile.ejs', {user});
 });
+
+
+// 0507 add a fake userid
 app.get('/index',function(req, res) {
-  res.render('index', { tasks });
+  //add-1
+  const userId=1;
+  // res.render('index', { tasks,userId });
+  const tasks = db.prepare(`SELECT * FROM task_list WHERE user_id=?`).all(userId);
+  res.render('index', { tasks,userId });
   });
+
+
 app.post('/addTask', function(req, res) {
     const task_title = req.body.taskName;
     const task_due_date = req.body.dueDate;
@@ -60,7 +69,8 @@ app.post('/addTask', function(req, res) {
     
 });
 app.get('/getTask', function(req, res) {
-const user_id = req.session.userId;
+// const user_id = req.session.userId;
+const user_id=1;
 const tasks = db.prepare(`SELECT * FROM task_list WHERE user_id=?`).all(user_id);
 res.json(tasks);
 });
@@ -170,4 +180,3 @@ const tasks = [
 
   //for css
   app.use('/public-styles', express.static(path.join(__dirname, 'public-styles'), { type: 'text/css' }));
-
